b52d8f44bf63b356f8e76f1f5b174d21338465c5
Goldpm <lowedavidadam@gmail.com>
2019-10-30 16:22:29 -0400

    added  logic that pulls weather api info to page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,19 @@ function initSearch() {
         url: "http://api.openweathermap.org/data/2.5/weather?apikey=166a433c57516f51dfab1f7edaed8413&q=" + searchParam + "&units=imperial",
         method: "GET"
     }).then(function (response) {
+        var today = new Date();
+        var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+        $('#cityName').html(response.name + " " + date)
+        $('#temp').html("Temperature: " + response.main.temp)
+        $('#humid').html("Humidity: " + response.main.humidity)
+        $('#windSpeed').html("Wind Speed: " + response.wind.speed)
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/uvi?appid=166a433c57516f51dfab1f7edaed8413&lat=" + response.coord.lat + "&lon=" + response.coord.lon + "",
+            method: "GET"
+        }).then(function (response) {
+            $('#uvIndex').html("UVIndex: " + response.value)
+        })
+
+
+    })
+}
