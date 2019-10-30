cf09af5e5ce30c80af134d228005903d88fa4ac9
Goldpm <lowedavidadam@gmail.com>
2019-10-30 16:21:36 -0400

    added ajax requset

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,2 +1,7 @@
 var history = [];
 function initSearch() {
+    var searchParam = $('#searchArea').val();
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?apikey=166a433c57516f51dfab1f7edaed8413&q=" + searchParam + "&units=imperial",
+        method: "GET"
+    }).then(function (response) {
